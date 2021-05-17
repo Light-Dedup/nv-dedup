941131947018121b36bb5f296122e3c594d7564b
hsiaochia <hsiaojiahuang@163.com>
2021-05-17 21:46:56 +0800

    add scripts to setup or remount etc.

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ ! $1 ]; then
+    timing=0
+else
+    timing=$1
+fi
+echo umounting...
+umount /mnt/pmem
+echo Removing the old kernel module...
+rmmod nova
+echo Inserting the new kernel module...
+insmod nova.ko measure_timing=$timing
+
+sleep 1
+
+echo mounting...
+mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
+#mount -t NOVA -o init /dev/pmem0 /mnt/pmem
