466e680b856822f34391c640a35bab71639810b8
Deadpoolmine <48178553+Deadpoolmine@users.noreply.github.com>
2022-06-04 22:12:44 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 sleep 5
 
 echo umounting...
-umount /mnt/pmem1
+umount /mnt/pmem0
 echo Removing the old kernel module...
 rmmod nova
 echo Inserting the new kernel module...
@@ -18,6 +18,6 @@ insmod nova.ko measure_timing=$timing
 sleep 1
 
 echo mounting...
-mount -t NOVA -o init -o wprotect,data_cow /dev/pmem1 /mnt/pmem1
+mount -t NOVA -o init -o wprotect,data_cow /dev/pmem0 /mnt/pmem0
 #mount -t NOVA -o init -o wprotect /dev/pmem0 /mnt/pmem
 #mount -t NOVA -o init /dev/pmem0 /mnt/pmem
