3ea32bb1337b91ad5dfb3b731a4d6cb2d279a7c1
deadpool <2841954631@qq.com>
2022-09-08 12:58:55 +0800

    only fill weak flag when there are no weak/strong entry in hlist

--- entry.c
+++ entry.c
@@ -111,13 +111,14 @@ static int nova_calc_non_fin(struct super_block *sb)
     struct nova_sb_info *sbi = NOVA_SB(sb);
     struct nova_pmm_entry *pentries, *pentry;
     struct nova_fp_weak fp_weak;
-    struct nova_fp_strong fp_strong;
+    // struct nova_fp_strong fp_strong;
     u32 weak_idx;
-    u64 strong_idx;
+    // u64 strong_idx;
     void *kmem;
     unsigned long idx;
     struct nova_hentry *hentry;
-    struct nova_hentry *weak_find_hentry, *strong_find_hentry;
+    struct nova_hentry *weak_find_hentry;
+    // struct nova_hentry  *strong_find_hentry;
     u64 blocknr;
 
     pentries = nova_get_block(sb, nova_get_block_off(sb, sbi->metadata_start, NOVA_BLOCK_TYPE_4K));
@@ -147,24 +148,8 @@ static int nova_calc_non_fin(struct super_block *sb)
 	            spin_lock(sbi->weak_hash_table_locks + weak_idx % HASH_TABLE_LOCK_NUM);
                 weak_find_hentry = nova_find_in_weak_hlist(sb, &sbi->weak_hash_table[weak_idx], &fp_weak);
                 if (weak_find_hentry) {
-                    nova_fp_strong_calc(&sbi->nova_fp_strong_ctx, kmem, &fp_strong);
-                    strong_idx = (fp_strong.u64s[0] & ((1 << sbi->num_entries_bits) - 1));
-                    spin_lock(sbi->strong_hash_table_locks + strong_idx % HASH_TABLE_LOCK_NUM);
-                    strong_find_hentry = nova_find_in_strong_hlist(sb, &sbi->strong_hash_table[strong_idx], &fp_strong);
-                    if(!strong_find_hentry) {
-                        pentry->flag = FP_STRONG_FLAG;
-                        pentry->fp_strong = fp_strong;
-                        pentry->fp_weak = fp_weak;
-                        nova_flush_buffer(pentry, sizeof(*pentry), true);
-                        hentry = nova_alloc_hentry(sb);
-                        hentry->entrynr = idx;
-                        hlist_add_head(&hentry->node, &sbi->strong_hash_table[strong_idx]);
-                    }
-                    else {
-                        /* non dedup this block now, or we must free the block, if this block is 
-                           referenced by file already, things get complex. */
-                    }
-                    spin_unlock(sbi->strong_hash_table_locks + strong_idx % HASH_TABLE_LOCK_NUM);
+                    /* non dedup this block now, or we must free the block, if this block is 
+                       referenced by file already, things get complex. */
                 } 
                 else {
                     pentry->flag = FP_WEAK_FLAG;
